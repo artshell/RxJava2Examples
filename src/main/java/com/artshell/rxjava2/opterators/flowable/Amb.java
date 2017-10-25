2f841623aa2f7a904b6db00ccc774b320298d708
artshell <872780008.qq.com>
2017-10-25 21:06:09 +0800

    Add: amb/ambArray/ambWith opterator examples

---
+++ src/main/java/com/artshell/rxjava2/opterators/flowable/Amb.java
@@ -0,0 +1,30 @@
+package com.artshell.rxjava2.opterators.flowable;
+
+import org.reactivestreams.Publisher;
+
+public class Amb {
+    /**
+     * @see io.reactivex.Flowable#amb(Iterable)
+     */
+    private static void amb() {
+
+    }
+
+    /**
+     * @see io.reactivex.Flowable#ambArray(Publisher[])
+     */
+    private static void ambArray() {
+
+    }
+
+    /**
+     * @see io.reactivex.Flowable#ambWith(Publisher)
+     */
+    private static void ambWith() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
